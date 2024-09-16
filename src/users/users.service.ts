aa7ad214e720dfa9232a727677965f3a54e1ee73
atiqBytes <atiqbytes@gmail.com>
2024-09-16 23:36:27 +0500

    created  a Data Transfer Object (DTO) to validate input when creating new users.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+      ) {}
+      
+      async findOne(username:string):Promise<User | undefined>
+      {
+        return this.usersRepository.findOne({where : {username}})
+      }
+
+      async create()
+}
+
