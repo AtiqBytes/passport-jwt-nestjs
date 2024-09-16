c933d15426c8bba4d7d524c1b2952e3661dc4481
atiqBytes <atiqbytes@gmail.com>
2024-09-17 00:00:37 +0500

    Update the AuthService to Work with TypeORM and BCrypt. our AuthService should validate user credentials and use bcrypt to compare passwords.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,19 @@
+import { UsersService } from './../users/users.service';
+import bcrypt from "bcrypt";
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+constructor(private userService:UsersService){}
+
+async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findOne(username);
+    if (user && await bcrypt.compare(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+}
