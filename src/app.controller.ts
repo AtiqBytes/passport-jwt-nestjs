5fe5e759e92ff86693fc8dbe49af7af9c510a04d
atiqBytes <atiqbytes@gmail.com>
2024-09-18 01:08:22 +0500

    add the 'refresh-token' enpoint in app controller to get access token when expired

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { User } from './users/user.entity';
-import { Controller, Get,Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get,Request, Post, UseGuards, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
@@ -21,5 +21,16 @@ export class AppController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @Post('refresh')
+  async refreshToken(@Body('refresh_token') refresh_token: string) {
+    const validUser = await this.authService.validateRefreshToken(refresh_token);
+    if (!validUser) {
+      return { statusCode: 401, message: 'Invalid refresh token' };
+    }
+    return {
+      access_token:await this.authService.generateAccessToken(validUser),
+    };
   }
 }
