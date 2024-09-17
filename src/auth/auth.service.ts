4b815c3debfa2752a8b54f6d6d23c34c73ff9c90
atiqBytes <atiqbytes@gmail.com>
2024-09-17 19:30:12 +0500

    we'll handle generating the JWT in the authService. Open the auth.service.ts file in the auth folder, and add the login() method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { UsersService } from './../users/users.service';
 import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-constructor(private userService:UsersService){}
+constructor(private userService:UsersService, private jwtService: JwtService){}
 
 async validateUser(username: string, password: string): Promise<any> {
     const user = await this.userService.findOne(username);
@@ -14,6 +15,13 @@ async validateUser(username: string, password: string): Promise<any> {
       return result;
     }
     return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 
 }
